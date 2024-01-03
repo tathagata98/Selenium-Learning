893017d0661e937c79cc92fae917642345db5974
Tathagata Pal <tathagatapal12@gmail.com>
2024-01-04 03:12:53 +0530

    Push

--- src/Practice/clearconsole.java
+++ src/Practice/clearconsole.java
@@ -4,15 +4,17 @@ package Practice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.DevToolsException;
 import org.openqa.selenium.devtools.v120.network.Network;
+import org.openqa.selenium.devtools.v120.network.model.LoaderId;
 import org.openqa.selenium.devtools.v120.network.model.RequestId;
 import org.openqa.selenium.devtools.v120.network.model.Response;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -20,12 +22,14 @@ import java.util.Set;
 public class clearconsole {
     int i = 1;
     int j = 1;
+    static String responses;
+    static String payload;
+
     static ChromeDriver driver;
 
     static DevTools dev;
-
+    static List<String> urls = new ArrayList<String>();
     Response response;
-
 
 
     public void clearConsoleErrors() {
@@ -33,61 +37,67 @@ public class clearconsole {
         String script = "console.clear();";
         js.executeScript(script);
     }
-    public void addcookies()
-    {
-        System.out.println("The cookies are :- ");
+
+    public void addcookies() {
         driver.manage().addCookie(new Cookie("profileid", "9999"));
+    }
+
+    public void displaycookies() {
+        System.out.println("The cookies are :- ");
+
         Set<Cookie> cookies = driver.manage().getCookies();
         String[] Cookie = cookies.toString().split(",");
         for (String print : Cookie)
-            System.out.println(" Cookie Details :-"+print);
+            System.out.println(" Cookie Details :-" + print);
 
     }
-    public WebDriver requestdetails(String value) {
+
+    public void requestdetails(String value) {
         dev = driver.getDevTools();
-        dev.createSession();
+        dev.createSessionIfThereIsNotOne();
         dev.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
         dev.addListener(Network.responseReceived(), r ->
-        {
-            response = r.getResponse();
-            if ((response.getUrl().endsWith(value)))
-            {
-                RequestId[] requestId = new RequestId[]{r.getRequestId()};
+                {
+                    response = r.getResponse();
+                    if ((response.getUrl().matches(value))) {
 
-                for (RequestId id : requestId) {
-                    String responses = null;
-                    try {
-                        responses = dev.send(Network.getResponseBody(id)).getBody();
-                    } catch (DevToolsException e)
-                    {
-                        e.printStackTrace();
+                        RequestId[] requestId = new RequestId[]{r.getRequestId()};
+                        for (RequestId id : requestId) {
+                            try {
+                                responses = dev.send(Network.getResponseBody(id)).getBody();
+                                payload = dev.send(Network.getRequestPostData(id));
+
+                            } catch (DevToolsException d) {
+                                d.getMessage();
+
+
+                                System.out.println("URL :" + j + " :-" + value);
+                                System.out.println("STATUS CODE :" + j + " :-" + response.getStatus());
+                                System.out.println("PAYLOAD" + j + " :-" + "=====" + payload);
+                                System.out.println("RESPONSE " + j + " :-" + "=====" + responses);
+                                System.out.println("==================================  "+j);
+
+                            }
                     }
-                    //String payload = dev.send(Network.getRequestPostData(id));
-                    System.out.println("URL :" + j + " :-" + response.getUrl());
-                    System.out.println("STATUS CODE :" + j + " :-" + response.getStatus());
-                    //System.out.println("PAYLOAD" + j + " :-" + "=====" + payload);
-                    System.out.println("RESPONSE " + j + " :-" + "=====" + responses);
-                    j++;
                 }
-            }
+            j++;
+
+
         });
-        return driver;
     }
+
     @Test
-    public void move(){
+    public void move() {
         System.setProperty("webdriver.chrome.driver", "E:\\SeleniumPracticeandLearn\\chromedriver.exe");
 
         driver = new ChromeDriver();
-        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS) ;
         System.out.println("The responses are : -");
-
-        requestdetails("quickLinks");
+        requestdetails("https://jumbo.zomato.com/event");
         clearConsoleErrors();
         driver.get("https://www.zomato.com/");
         addcookies();
-        requestdetails("check-auth");
+        requestdetails("https://jumbo.zomato.com/event");
         driver.findElement(By.xpath("//a[contains(text(),'Add restaurant')]")).click();
-
+        displaycookies();
     }
-
 }
